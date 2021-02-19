b540bb0121991666b472eaa43ab52215cb01cfa6
柯慕灵 <1985312383@qq.com>
2021-02-19 21:29:45 +0800

    可生成TXT文件

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,8 +1,7 @@
-import os
-import re
 import random
 import readJSON
 import jieba
+import docx
 
 data = readJSON.readJson("data.json")
 nonsense = data['nonsense']
@@ -44,8 +43,11 @@ def get_subject_content():
 
 
 def get_content():
+    # doc = docx.Document()  # 以默认模板建立文档对象
+    # doc = docx.Document('论文.docx')  # 读取a.docx文档，建立文档对象
+
     tmp = str()  # 存放生成的文字
-    tmp += "标题：" + theme + "\n\t"
+    tmp += "标题：" + theme + "\n"
 
     # 添加摘要
     tmp += "摘要\n"
@@ -90,13 +92,27 @@ def get_content():
     for i in range(8):
         tmp += "\t" + "[" + str(i + 1) + "] " + next(random_reference) + ".\n"
 
-    # 打印全文
-    print(tmp)
+    # 打印全文，搞开发的小伙伴把下面的这话行代码取消注释就可以看控制台输出了
+    # print(tmp)
+
+    return tmp
 
 
 if __name__ == "__main__":
     theme = input("请输入文章主题：")  # 全文反复横跳的主题
     # type = input("请输入论文类型") #确定调用哪个种类的语论文包
     words = int(input("大概多少字："))  # 论文字数
-    get_content()
-    # 输出所生成论文
+
+    # 输出所生成论文,生成docx文件
+    tmp = get_content()
+
+    # 输出TXT文件
+    while txt != "y" or "n":
+        txt = input("是否生成txt文件在当前目录下？(y/n)")
+        if txt == "y":
+            file = open('论文.txt', 'w')
+            file.write(tmp)
+        elif txt == "n":
+            break
+        else:
+            print("请输入y或n!")
